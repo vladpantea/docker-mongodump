15fdef14b141b8ede8fbebaf10f8d949755d0749
unknown <vlad.pantea@gmail.com>
2018-01-14 23:02:40 +0200

    added mongo 3.6 and changed backup folder location

---
+++ 3.6/backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+echo "Job started: $(date)"
+
+DATE=$(date +%Y%m%d_%H%M%S)
+FILE="/data/backup/backup-$DATE.tar.gz"
+
+mkdir -p dump
+mongodump -h $MONGO_HOST -p $MONGO_PORT
+tar -zcvf $FILE dump/
+rm -rf dump/
+
+echo "Job finished: $(date)"
